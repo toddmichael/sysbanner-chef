9d44dd9eec0a71676a29f8609f801ec1aef39624
Todd Michael Bushnell <toddmichael@gmail.com>
2015-03-07 09:51:42 -0800

    sane attribute examples

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
-default[:sysbanner][:org_name] = 'Foo, Inc'
+default[:sysbanner][:org_name] = 'Little Company, Inc'
 
 # AWS attributes
-default[:sysbanner][:instance_type] = 'n/a'
-default[:sysbanner][:security_groups] = 'n/a' 
-default[:sysbanner][:private_ip] = 'n/a' 
-default[:sysbanner][:public_ip] = 'n/a' 
+default[:sysbanner][:instance_type] = 't1.micro'
+default[:sysbanner][:security_groups] = %w(group-1 group-2) 
+default[:sysbanner][:private_ip] = '192.168.0.253' 
+default[:sysbanner][:public_ip] = '172.16.0.253' 
