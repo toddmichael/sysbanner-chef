98bd95befb41f49e62aa73af1b830a2dbed55135
Todd Michael Bushnell <toddmichael@gmail.com>
2015-03-16 22:35:49 -0700

    bump version to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Todd Michael Bushnell'
 license 'Apache 2.0'
 description 'Distribute system banner files'
 description 'Distribute system banner files'
-version '0.1.0'
+version '0.1.1'
 
 supports 'centos'
 supports 'ubuntu', '>= 14.04'
