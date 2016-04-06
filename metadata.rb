471a09485ea3dbd8dcf5afe7673f275cb56d4f6a
Todd Michael Bushnell <toddmichael@gmail.com>
2016-04-05 21:20:14 -0700

    add source and issues urls cuz foodcritic asked me to

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,9 @@ name 'sysbanner'
 maintainer 'Todd Michael Bushnell'
 license 'Apache 2.0'
 description 'Distribute system banner files'
-description 'Distribute system banner files'
+long_description 'Distribute system banner files'
+source_url 'https://github.com/toddmichael/sysbanner-chef' if respond_to?(:source_url)
+issues_url 'https://github.com/toddmichael/sysbanner-chef/issues' if respond_to?(:issues_url)
 version '0.1.1'
 
 supports 'centos'
