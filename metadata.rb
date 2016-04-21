af681d8f4012349f2b9b7ab363ad516a3c94b6af
Todd Michael Bushnell <toddmichael@gmail.com>
2016-04-21 14:19:32 -0700

    bump to 0.1.3

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ description 'Distribute system banner files'
 long_description 'Distribute system banner files'
 source_url 'https://github.com/toddmichael/sysbanner-chef' if respond_to?(:source_url)
 issues_url 'https://github.com/toddmichael/sysbanner-chef/issues' if respond_to?(:issues_url)
-version '0.1.2'
+version '0.1.3'
 
 supports 'centos'
 supports 'ubuntu', '>= 14.04'
